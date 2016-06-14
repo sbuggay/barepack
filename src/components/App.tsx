69c66a432fe27d40b77d5ca964af3369afc638f1
Devan Buggay <debuggay@microsoft.com>
2016-06-14 16:57:19 -0700

    Cleaned up components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
 import TabControl from './TabControl';
-import TableContainer from './TableContainer';
+import TableContainer from './contacts/TableContainer';
 
 class App extends React.Component<any, any> {
     render() {
         return (
             <div>
                 <TabControl/>
-                <TableContainer/>
+
             </div>
         );
     }
